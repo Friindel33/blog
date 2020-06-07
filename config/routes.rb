3c493be41dcd5d1ae61e6515723db2300e46bc3f
Juri F <friindel@mail.ru>
2020-06-07 14:35:10 +0100

    contact controller/route changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 resources :articles
-resources :contacts
-
-
-
+resource :contacts, only: [:new, :create]
 end
