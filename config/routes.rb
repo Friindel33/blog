101be6fe09872b09fb49ad13b7c16c6cd122745e
Juri F <friindel@mail.ru>
2020-06-09 13:42:12 +0100

    implement articles#create

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 resources :articles
-resource :contacts, only: [:new, :create]
+resource :contacts, only: [:new, :create], path_names: { :new => '' }
 end
