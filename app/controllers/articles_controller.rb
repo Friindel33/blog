d4cbdb4d48d52c699ccc14e40008934f0ff0a1df
Juri F <friindel@mail.ru>
2020-06-09 18:12:08 +0100

    lesson 40

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,45 @@
 class ArticlesController < ApplicationController
 
-  def new
+  def index
+    @articles = Article.all
+  end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
   end
 
   def create
     @article = Article.new(article_params)
-    if @article.valid?
-      @article.save
+    if @article.save
+      redirect_to @article
     else
       render action: 'new'
     end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+      @article=Article.find(params[:id])
+      @article.destroy
+
+      flash.notice="Article '#{@article.title}' was deleted"
+
+      redirect_to articles_path
   end
 
 private
