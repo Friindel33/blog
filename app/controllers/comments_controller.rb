99a7ceb53ba1b2ca8040528796e4323d822532ca
Juri F <friindel@mail.ru>
2020-06-17 18:43:24 +0100

    minor bootstrap view changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,8 @@ class CommentsController < ApplicationController
 
   def create
     @article = Article.find(params[:article_id])
-    @comments = @article.comments.create(comment_params)
+    @comments = @article.comments.new(comment_params)
+    @comments.save
 
     redirect_to article_path(@article)
   end
