05cb34c8629645e7d75172591df26a97e6eb8d8a
Juri F <friindel@mail.ru>
2020-06-24 13:02:55 +0100

    updated factory article with comments

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,15 @@ FactoryBot.define do
   factory :article do
     title { "Article title" }
     text { "Article text" }
+
+    # creating factory "article with comments"
+    # for creating article with many comments
+    factory :article_with_comments do
+      # after creatin article
+      after :create do |article, evaluator|
+        #creating list with 3 comments
+        create_list :comment, 3, article: article
+      end
+    end
   end
 end
