8cac8432fd140462d882ddfc2d3f73e9d15a549e
Juri F <friindel@mail.ru>
2020-06-24 14:50:59 +0100

    Added comment body length spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 describe Comment do
-  it { should belong_to :article }
+  describe "belonging" do
+    it { should belong_to :article }
+  end
+  describe "length" do
+    it { should validate_length_of(:body).is_at_most(300) }
+  end
 end
