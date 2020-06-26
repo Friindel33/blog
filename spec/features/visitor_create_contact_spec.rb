bb9065735a3d4927d34dabbf745d4cef8e8c483a
Juri F <friindel@mail.ru>
2020-06-26 15:46:07 +0100

    Contact creation form success test

---
+++ spec/features/visitor_create_contact_spec.rb
@@ -0,0 +1,20 @@
+# testing create contact feature
+require "rails_helper"
+
+feature "Contact creation" do
+  scenario "allows access to contacts page" do
+    visit '/contacts'
+
+    expect(page).to have_content I18n.t'contacts.contact_us'
+  end
+
+  scenario "allows guest to create contact" do
+    visit '/contacts'
+
+    fill_in :contact_email, :with => 'user@example.com'
+    fill_in :contact_message, :with => 'something'
+
+    click_button 'Send Message'
+    expect(page).to have_content 'Thanks for Your message', 'We will contact You as soon as possible'
+  end
+end
